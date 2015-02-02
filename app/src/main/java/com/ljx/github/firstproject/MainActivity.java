e8b28a107737bd7d8921de0ba08ccb5de9f1ecb8
ljxgithub <1656610165@qq.com>
2015-02-02 17:26:48 +0800

    抄Launcher PagedView

---
+++ app/src/main/java/com/ljx/github/firstproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ljx.github.firstproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ljx on 2015/2/2.
+ */
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_layout);
+	}
+}
